bf2f338eb85e1f328c609c566095716c96408841
Jay  Shah <211650@India.TCS.com>
2017-05-18 14:27:26 +0530

    First Commit

---
+++ osdemo/src/main/java/com/example/DemoApplication.java
@@ -0,0 +1,27 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@SpringBootApplication
+@EnableGlobalMethodSecurity
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+}
+
+@RestController
+class EquifaxController{
+	
+	  	
+  @RequestMapping("/")
+  String message(){
+    return  "Hello Equifax!";
+  }
+}
